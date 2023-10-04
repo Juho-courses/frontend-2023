d522fe73f8ec994eacb0446855459d6078945599
Juho S <juho.salli@gmail.com>
2023-10-04 12:54:31 +0300

    lisää 02-komponentteja

---
+++ 02-komponentteja/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Band from './Band.svelte';
+
+	let kiss = {
+		name: 'Kiss',
+		genre: 'rock',
+		member_count: 15
+	};
+</script>
+
+<h1>Bändejä</h1>
+
+<Band {...kiss} />
+<!-- <Band band_data={pantera} /> -->
+<Band name="Pantera" genre="metal" member_count={4} />
+<!-- <Band name="12" genre="hard rock" /> -->
---
+++ 02-komponentteja/src/routes/Band.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let name;
+	export let genre;
+	export let member_count = 0;
+
+	let crew_size = member_count * 2;
+
+	// export let band_data;
+</script>
+
+<div>
+	<h3>{name}</h3>
+	<p>Genre: {genre}</p>
+	<p>Jäsenten määrä: {member_count} (crew size: {crew_size})</p>
+</div>
+
+<style>
+	div {
+		border: 1px solid black;
+		margin-bottom: 1rem;
+	}
+</style>
