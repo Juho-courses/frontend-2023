85627e0a47cdd699502c4e0039b532b320f317b4
Juho S <juho.salli@gmail.com>
2023-10-04 12:54:12 +0300

    lisää 01-eka-sveltekit

---
+++ 01-eka-sveltekit/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Lista from './Lista.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<Lista />
+<Lista />
+<Lista />
+<Lista />
+<Lista />
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
---
+++ 01-eka-sveltekit/src/routes/Lista.svelte
@@ -0,0 +1,11 @@
+<script>
+	let nimi = 'Opintojaksot';
+</script>
+
+<h1>{nimi} Lista</h1>
+
+<ul>
+	<li>1</li>
+	<li>2</li>
+	<li>3</li>
+</ul>
