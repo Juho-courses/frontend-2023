b60cca005df0e115aa3407a2c06de411aa1031c0
Juho S <juho.salli@gmail.com>
2023-11-22 11:19:26 +0200

    logitus pois layout.js

--- luentokoodit/07-datan-hakeminen/src/routes/+layout.js
+++ luentokoodit/07-datan-hakeminen/src/routes/+layout.js
@@ -1,5 +1,4 @@
 export async function load({ fetch }) {
-	console.log('layout.js');
 	const res = await fetch('https://band-api.netlify.app/.netlify/functions/get-bands');
 	const data = await res.json();
 	return { bands: data };
